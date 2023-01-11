d47d48fdea5662ea082509473e227d2ea1b1fa49
Steve Butler <stevebutler11@gmail.com>
2023-01-11 09:02:38 +0000

    CMake min version changed, main , cmakelists.txt and main components edited to same config as a projucer blank audio app project

--- MainComponent.h
+++ MainComponent.h
@@ -4,21 +4,28 @@
 // directly. If you need to remain compatible with Projucer-generated builds, and
 // have called `juce_generate_juce_header(<thisTarget>)` in your CMakeLists.txt,
 // you could `#include <JuceHeader.h>` here instead, to make all your module headers visible.
-#include <juce_gui_extra/juce_gui_extra.h>
+//#include <juce_gui_extra/juce_gui_extra.h>
+#include <JuceHeader.h>
 
 //==============================================================================
 /*
     This component lives inside our window, and this is where you should put all
     your controls and content.
 */
-class MainComponent   : public juce::Component
+class MainComponent : public juce::AudioAppComponent
 {
 public:
     //==============================================================================
     MainComponent();
+    ~MainComponent() override;
 
     //==============================================================================
-    void paint (juce::Graphics&) override;
+    void prepareToPlay (int samplesPerBlockExpected, double sampleRate) override;
+    void getNextAudioBlock (const juce::AudioSourceChannelInfo& bufferToFill) override;
+    void releaseResources() override;
+
+    //==============================================================================
+    void paint (juce::Graphics& g) override;
     void resized() override;
 
 private:
