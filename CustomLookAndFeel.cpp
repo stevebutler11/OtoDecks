f7882bf7785dd1b1a8812dffa59af99add6589a3
steve <stevebutler11@gmail.com>
2023-03-02 11:26:15 +0000

    slider thumb improved

--- CustomLookAndFeel.cpp
+++ CustomLookAndFeel.cpp
@@ -77,7 +77,7 @@ void CustomLookAndFeel::drawLinearSlider(Graphics& g, int x, int y, int width, i
         backgroundTrack.startNewSubPath (startPoint);
         backgroundTrack.lineTo (endPoint);
         g.setColour (slider.findColour (Slider::backgroundColourId));
-        g.strokePath (backgroundTrack, { trackWidth, PathStrokeType::curved, PathStrokeType::rounded });
+        g.strokePath (backgroundTrack, { trackWidth, PathStrokeType::curved, PathStrokeType::square });
 
         Path valueTrack;
         Point<float> minPoint, maxPoint, thumbPoint;
@@ -108,12 +108,64 @@ void CustomLookAndFeel::drawLinearSlider(Graphics& g, int x, int y, int width, i
         valueTrack.startNewSubPath (minPoint);
         valueTrack.lineTo (isThreeVal ? thumbPoint : maxPoint);
         g.setColour (slider.findColour (Slider::trackColourId));
-        g.strokePath (valueTrack, { trackWidth, PathStrokeType::curved, PathStrokeType::rounded });
+        g.strokePath (valueTrack, { trackWidth, PathStrokeType::curved, PathStrokeType::square });
 
         if (! isTwoVal)
         {
-            g.setColour (slider.findColour (Slider::thumbColourId));
-            g.fillEllipse (Rectangle<float> (static_cast<float> (thumbWidth), static_cast<float> (thumbWidth)).withCentre (isThreeVal ? thumbPoint : maxPoint));
+            if (style == Slider::SliderStyle::LinearVertical)
+            {
+                auto thumbRect = Rectangle<float> (
+                static_cast<float> (thumbWidth * 3),
+                        static_cast<float> (thumbWidth * 2)
+                ).withCentre (isThreeVal ? thumbPoint : maxPoint);
+
+                auto cg = ColourGradient(Colour::fromString("#FF333333"),
+                                         thumbRect.getX(), thumbRect.getY(),
+                                         Colour::fromString("#FF333333"),
+                                         thumbRect.getX(), thumbRect.getY() + thumbRect.getHeight(),
+                                         false);
+
+                cg.addColour(0.4, findColour(juce::ResizableWindow::backgroundColourId));
+                cg.addColour(0.6, findColour(ListBox::ColourIds::backgroundColourId));
+
+                g.setGradientFill(cg);
+                g.fillRect(thumbRect);
+
+                g.setColour(findColour(TextButton::ColourIds::textColourOffId));
+                g.drawLine(
+                        thumbRect.getX(),
+                        thumbRect.getCentreY(),
+                        thumbRect.getX() + (thumbWidth * 3),
+                        thumbRect.getCentreY(),
+                        1);
+            }
+            if (style == Slider::SliderStyle::LinearHorizontal)
+            {
+                auto thumbRect = Rectangle<float> (
+                        static_cast<float> (thumbWidth * 2),
+                        static_cast<float> (thumbWidth * 3)
+                ).withCentre (isThreeVal ? thumbPoint : maxPoint);
+
+                auto cg = ColourGradient(Colour::fromString("#FF333333"),
+                                         thumbRect.getX(), thumbRect.getY(),
+                                         Colour::fromString("#FF333333"),
+                                         thumbRect.getX() + thumbRect.getWidth(), thumbRect.getY(),
+                                         false);
+
+                cg.addColour(0.4, findColour(juce::ResizableWindow::backgroundColourId));
+                cg.addColour(0.6, findColour(ListBox::ColourIds::backgroundColourId));
+
+                g.setGradientFill(cg);
+                g.fillRect(thumbRect);
+
+                g.setColour(findColour(TextButton::ColourIds::textColourOffId));
+                g.drawLine(
+                        thumbRect.getCentreX(),
+                        thumbRect.getY(),
+                        thumbRect.getCentreX(),
+                        thumbRect.getY() + (thumbWidth * 3),
+                        1);
+            }
         }
 
         if (isTwoVal || isThreeVal)
