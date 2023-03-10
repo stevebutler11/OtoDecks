40f60d8544c0cfdd947cf955459a31356806892d
steve <stevebutler11@gmail.com>
2023-03-10 14:02:17 +0000

    TrackInfoDisplay tidied

--- TrackInfoDisplay.cpp
+++ TrackInfoDisplay.cpp
@@ -1,75 +1,60 @@
 #include "TrackInfoDisplay.h"
 
-#include <utility>
-
-TrackInfoDisplay::TrackInfoDisplay()
-{
+TrackInfoDisplay::TrackInfoDisplay() {
     startTimer(200);
 }
 
-TrackInfoDisplay::~TrackInfoDisplay()
-{
+TrackInfoDisplay::~TrackInfoDisplay() {}
 
-}
-
-void TrackInfoDisplay::paint(Graphics& g)
-{
-    g.fillAll (getLookAndFeel().findColour(ListBox::ColourIds::backgroundColourId));
+void TrackInfoDisplay::paint(Graphics &g) {
+    g.fillAll(getLookAndFeel().findColour(ListBox::ColourIds::backgroundColourId));
     g.setColour(getLookAndFeel().findColour(TextButton::ColourIds::textColourOffId));
     Typeface::Ptr tface = Typeface::createSystemTypefaceFor(BinaryData::DSDIGI_TTF, BinaryData::DSDIGI_TTFSize);
     g.setFont(Font(tface));
-    g.setFont (24.0f);
+    g.setFont(24.0f);
 
-    if (displayTextWidth == 0)
-    {
+    if (displayTextWidth == 0) {
         displayTextWidth = g.getCurrentFont().getStringWidthFloat(displayText);
         whitespaceCharWidth = g.getCurrentFont().getStringWidthFloat(" ");
         padDisplayText();
     }
 
-    g.drawText (paddedText, getLocalBounds(), juce::Justification::centredLeft, false);
+    g.drawText(paddedText, getLocalBounds(), Justification::centredLeft, false);
 }
 
-void TrackInfoDisplay::resized()
-{
+void TrackInfoDisplay::resized() {
     padDisplayText();
 }
 
-void TrackInfoDisplay::timerCallback()
-{
+void TrackInfoDisplay::timerCallback() {
+    // every callback, move one character to the left
     auto first_char = paddedText.front();
     paddedText = paddedText.substr(1) += first_char;
     repaint();
 }
 
-void TrackInfoDisplay::padDisplayText()
-{
+void TrackInfoDisplay::padDisplayText() {
     paddedText = displayText;
 
-    if (displayTextWidth > 0 && whitespaceCharWidth > 0)
-    {
+    if (displayTextWidth > 0 && whitespaceCharWidth > 0) {
         // calculate how many spaces are needed to reach end of screen
         auto remainingSpace = (float) getLocalBounds().getWidth() - displayTextWidth;
-        if (remainingSpace > 0)
-        {
+        if (remainingSpace > 0) {
             int padNumber = remainingSpace / whitespaceCharWidth;
 
             // pad the text the correct amount
-            for (auto i = 0; i < padNumber; ++i)
-            {
+            for (auto i = 0; i < padNumber; ++i) {
                 paddedText += " ";
             }
         }
     }
 }
 
-void TrackInfoDisplay::setText(std::string trackText)
-{
+void TrackInfoDisplay::setText(std::string trackText) {
     // strip leading & trailing whitespace
     auto startIndex = trackText.find_first_not_of(' ');
     auto endIndex = trackText.find_last_not_of(' ');
-    if (startIndex != std::string::npos && endIndex != std::string::npos)
-    {
+    if (startIndex != std::string::npos && endIndex != std::string::npos) {
         trackText = trackText.substr(startIndex, endIndex + 1);
     }
 
--- TrackInfoDisplay.h
+++ TrackInfoDisplay.h
@@ -3,24 +3,67 @@
 
 #include <JuceHeader.h>
 
-class TrackInfoDisplay : public Component, Timer
-{
+/**
+ * A class to display the track information on the DJ deck
+ */
+class TrackInfoDisplay : public Component, Timer {
 public:
+
+    /**
+     * TrackInfoDisplay constructor
+     */
     TrackInfoDisplay();
+
+    /**
+     * TrackInfoDisplay destructor
+     */
     ~TrackInfoDisplay() override;
 
+    /**
+     * Sets the track detail text to scroll across the screen
+     * @param trackText the track detail text
+     */
     void setText(std::string trackText);
+
     //================ Component pure virtual functions ================
-    void paint(Graphics& g) override;
+
+    /**
+     * Components can override this method to draw their content
+     * @param g the graphics context that must be used to do the drawing operations
+     */
+    void paint(Graphics &g) override;
+
+    /**
+     * Called when this component's size has been changed
+     */
     void resized() override;
 
     //================ Timer pure virtual functions ================
+
+    /**
+     * The user-defined callback routine that actually gets called periodically
+     */
     void timerCallback() override;
+
 private:
+
+    /**
+     * pads the string of text with the correct amount of whitespace for the current TrackInfoDisplay width so that it
+     * scrolls off of the screen smoothly
+     */
     void padDisplayText();
+
+    // The initial text (this is **NOT** adjusted for screen whitespace)
     std::string displayText{"File not loaded..."};
+
+    // The current width of the text to be displayed (pre-padding)
     float displayTextWidth{0};
+
+    // The width of a single whitespace character
     float whitespaceCharWidth{0};
+
+    // The text to be displayed on screen (this is the text adjusted for screen whitespace)
     std::string paddedText{displayText};
 };
+
 #endif //GUI_APP_EXAMPLE_TRACKINFODISPLAY_H
