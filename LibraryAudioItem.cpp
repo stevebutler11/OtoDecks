ec8b8977fa695834e200eb938d0ebca7b6863a49
steve <stevebutler11@gmail.com>
2023-03-10 13:44:54 +0000

    LibraryAudioItem tidied

--- LibraryAudioItem.cpp
+++ LibraryAudioItem.cpp
@@ -1,69 +1,60 @@
 #include "LibraryAudioItem.h"
 
-LibraryAudioItem::LibraryAudioItem(File& _file,
-                                   std::string& _fileName,
+LibraryAudioItem::LibraryAudioItem(File &_file,
+                                   std::string &_fileName,
                                    double _duration,
-                                   std::string& _extension,
+                                   std::string &_extension,
                                    std::string _key,
                                    double _bpm
-                                    ) :file(_file),
-                                    fileName(_fileName),
-                                    duration(_duration),
-                                    extension(_extension),
-                                    key((std::string &) _key),
-                                    BPM(_bpm)
-{}
+) : file(_file),
+    fileName(_fileName),
+    duration(_duration),
+    extension(_extension),
+    key((std::string &) _key),
+    BPM(_bpm) {}
 
-LibraryAudioItem::~LibraryAudioItem()
-{
+LibraryAudioItem::~LibraryAudioItem() {
 
 }
 
-void LibraryAudioItem::setKey(std::string& _key)
-{
+void LibraryAudioItem::setKey(std::string &_key) {
     key = _key;
 }
 
-void LibraryAudioItem::setBPM(double _BPM)
-{
+void LibraryAudioItem::setBPM(double _BPM) {
     BPM = _BPM;
 }
 
-File LibraryAudioItem::getFile()
-{
+File LibraryAudioItem::getFile() {
     return file;
 }
 
-std::string LibraryAudioItem::getFileName() const
-{
+std::string LibraryAudioItem::getFileName() const {
     return fileName;
 }
 
-double LibraryAudioItem::getDuration() const
-{
+double LibraryAudioItem::getDuration() const {
     return duration;
 }
 
-std::string LibraryAudioItem::getDurationFormatted() const
-{
-    //TODO: zero pad the seconds
-    int mins = duration / 60;
-    int remaining_secs = std::fmod(duration, 60);
+std::string LibraryAudioItem::getDurationFormatted() const {
+    auto mins = std::to_string((int) duration / 60);
+    auto remaining_secs = std::to_string((int) std::fmod(duration, 60));
 
-    return std::to_string(mins) + ":" + std::to_string(remaining_secs) + "s";
+    mins.insert(mins.begin(), (int) 2 - mins.size(), '0');
+    remaining_secs.insert(remaining_secs.begin(), (int) 2 - remaining_secs.size(), '0');
+
+    return mins + ":" + remaining_secs + "s";
 }
 
-std::string LibraryAudioItem::getExtension() const
-{
+std::string LibraryAudioItem::getExtension() const {
     return extension;
 }
 
-std::string LibraryAudioItem::getKey() const
-{
+std::string LibraryAudioItem::getKey() const {
     return key;
 }
 
-double LibraryAudioItem::getBPM() const
-{
+double LibraryAudioItem::getBPM() const {
     return BPM;
 }
--- LibraryAudioItem.h
+++ LibraryAudioItem.h
@@ -5,28 +5,82 @@
 #include <string>
 #include <vector>
 
-class LibraryAudioItem
-{
+/**
+ * Class that represents an audio item within the library component
+ */
+class LibraryAudioItem {
 public:
-    LibraryAudioItem(File& file, std::string& fileName, double duration, std::string& extension,
+
+    /**
+     * LibraryAudioItem constructor
+     * @param file Represents a local file or directory
+     * @param fileName the name of the file
+     * @param duration duration of the track in seconds
+     * @param extension file extension
+     * @param _key
+     * @param _bpm
+     */
+    LibraryAudioItem(File &file, std::string &fileName, double duration, std::string &extension,
                      std::string _key = "", double _bpm = 0.0);
+
+    /**
+     * LibraryAudioItem destructor
+     */
     ~LibraryAudioItem();
 
-    void setKey(std::string& key);
+    void setKey(std::string &key);
+
     void setBPM(double bpm);
+
+    /**
+     * Get the File of the LibraryAudioItem
+     * @return JUCE File that is returned
+     */
     File getFile();
+
+    /**
+     * Get the filename of the LibraryAudioItem
+     * @return name of the file
+     */
     std::string getFileName() const;
+
+    /**
+     * Get the duration of the LibraryAudioItem
+     * @return duration in seconds
+     */
     double getDuration() const;
+
+    /**
+     * Get the duration of the LibraryAudioItem as a std::string, formatted correctly
+     * @return formatted duration string
+     */
     std::string getDurationFormatted() const;
+
+    /**
+     * Get the extension of the LibraryAudioItem
+     * @return extension
+     */
     std::string getExtension() const;
+
     std::string getKey() const;
+
     double getBPM() const;
+
 private:
+
+    // Represents a local file or directory
     File file;
+
+    // name of the file
     std::string fileName;
+
+    // duration of the song in seconds
     double duration{0.0};
+
+    // extension of the JUCE File
     std::string extension;
-    //TODO: enum for key?
+
+
     std::string key;
     double BPM;
 };
