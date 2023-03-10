efbb219d8ddbc0ed3806a89a4d791051123332e1
steve <stevebutler11@gmail.com>
2023-03-10 08:21:49 +0000

    DeckGUI tidied

--- DeckGUI.cpp
+++ DeckGUI.cpp
@@ -1,12 +1,11 @@
 #include "DeckGUI.h"
 
 
-
-DeckGUI::DeckGUI(DJAudioPlayer* _player,
+DeckGUI::DeckGUI(DJAudioPlayer *_player,
                  AudioFormatManager &formatManagerToUse,
                  AudioThumbnailCache &cacheToUse
-                 ) : player(_player),
-                     waveformDisplay(_player, formatManagerToUse, cacheToUse) // pass cstr args directly to wavfrmDisp
+) : player(_player),
+    waveformDisplay(_player, formatManagerToUse, cacheToUse) // pass cstr args directly to wavfrmDisp
 {
     addAndMakeVisible(playPauseButton);
     addAndMakeVisible(cueButton);
@@ -20,6 +19,7 @@ DeckGUI::DeckGUI(DJAudioPlayer* _player,
     cueButton.addListener(this);
     tempoSlider.addListener(this);
 
+    // tempo ranges +/- 8%
     tempoSlider.setRange(0.92, 1.08);
     tempoSlider.setValue(1.0);
 
@@ -28,114 +28,89 @@ DeckGUI::DeckGUI(DJAudioPlayer* _player,
     startTimer(TIMER_INTERVAL);
 }
 
-DeckGUI::~DeckGUI()
-{
+DeckGUI::~DeckGUI() {
     stopTimer();
 }
 
-void DeckGUI::loadFile(File& file)
-{
+void DeckGUI::loadFile(File &file) {
     player->loadFile(file);
-    //TODO: change waveform to load from file rather than URL
     waveformDisplay.loadURL(URL{file});
     trackInfoDisplay.setText(file.getFileName().toStdString());
 }
 
-void DeckGUI::paint(Graphics& g)
-{
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+void DeckGUI::paint(Graphics &g) {
+    g.fillAll(getLookAndFeel().findColour(ResizableWindow::backgroundColourId));
 }
 
-void DeckGUI::resized()
-{
-    int rowH = getHeight()/12;
-    int colW = getWidth()/5;
+void DeckGUI::resized() {
+    int rowH = getHeight() / 12;
+    int colW = getWidth() / 5;
 
-    trackInfoDisplay.setBounds(0, 0, colW*5, rowH);
-    waveformDisplay.setBounds(0, rowH, colW*5, rowH*2);
+    trackInfoDisplay.setBounds(0, 0, colW * 5, rowH);
+    waveformDisplay.setBounds(0, rowH, colW * 5, rowH * 2);
 
-    cueButton.setBounds(0, rowH * 8, colW, rowH*2);
-    playPauseButton.setBounds(0, rowH * 10, colW, rowH*2);
+    cueButton.setBounds(0, rowH * 8, colW, rowH * 2);
+    playPauseButton.setBounds(0, rowH * 10, colW, rowH * 2);
 
-    tempoSlider.setBounds(colW * 4, rowH*3, colW, (int) (rowH * 8.5));
-    tempoLabel.setBounds(colW * 4, rowH*11, colW, rowH);
+    tempoSlider.setBounds(colW * 4, rowH * 3, colW, (int) (rowH * 8.5));
+    tempoLabel.setBounds(colW * 4, rowH * 11, colW, rowH);
     jogWheel.setBounds(colW, rowH * 3, colW * 3, rowH * 9);
 }
 
-void DeckGUI::buttonClicked(Button *button)
-{
-    if (button == &playPauseButton)
-    {
+void DeckGUI::buttonClicked(Button *button) {
+    if (button == &playPauseButton) {
         player->isPlaying() ? player->stop() : player->start();
     }
-    if (button == &cueButton)
-    {
+    if (button == &cueButton) {
         player->setPositionRelative(waveformDisplay.getCuePositionRelative());
         player->start();
     }
 }
 
-void DeckGUI::sliderValueChanged(Slider *slider)
-{
-    if (slider == &tempoSlider)
-    {
+void DeckGUI::sliderValueChanged(Slider *slider) {
+    if (slider == &tempoSlider) {
         *tempoVal = tempoSlider.getValue();
         player->setSpeed(tempoSlider.getValue());
     }
 }
 
-bool DeckGUI::isInterestedInFileDrag(const StringArray &files)
-{
+bool DeckGUI::isInterestedInFileDrag(const StringArray &files) {
     return true;
 }
 
-void DeckGUI::filesDropped(const StringArray &files, int x, int y)
-{
-    if (files.size() == 1)
-    {
+void DeckGUI::filesDropped(const StringArray &files, int x, int y) {
+    if (files.size() == 1) {
         auto file = File{files[0]};
-        //TODO include more file types?
-        if (file.getFileExtension() == ".mp3")
-        {
+        if (file.hasFileExtension(".mp3")) {
             loadFile(file);
-        }
-        else
-        {
+        } else {
             std::cout << "incorrect file type" << std::endl;
         }
-    }
-    else
-    {
+    } else {
         std::cout << "too many files" << std::endl;
     }
 }
 
-bool DeckGUI::isInterestedInDragSource(const SourceDetails &dragSourceDetails)
-{
+bool DeckGUI::isInterestedInDragSource(const SourceDetails &dragSourceDetails) {
     return true;
 }
 
-void DeckGUI::itemDropped(const SourceDetails &dragSourceDetails)
-{
+void DeckGUI::itemDropped(const SourceDetails &dragSourceDetails) {
     auto file = File{dragSourceDetails.description.toString()};
-    if (file.existsAsFile())
-    {
-        //TODO: check if mp3?
+    if (file.existsAsFile() && file.hasFileExtension(".mp3")) {
         loadFile(file);
     }
     repaint();
 }
 
-void DeckGUI::timerCallback()
-{
+void DeckGUI::timerCallback() {
     waveformDisplay.setPositionRelative(player->getPositionRelative());
 }
 
-void DeckGUI::formatLabel(Label& label, std::string text)
-{
+void DeckGUI::formatLabel(Label &label, std::string text) {
     Typeface::Ptr tface = Typeface::createSystemTypefaceFor(BinaryData::neuropol_otf, BinaryData::neuropol_otfSize);
-    label.setText (text, juce::dontSendNotification);
+    label.setText(text, dontSendNotification);
     label.setFont(Font(tface).withHeight(10.0f));
-    label.setColour (juce::Label::textColourId, juce::Colours::grey);
-    label.setJustificationType (juce::Justification::centredBottom);
+    label.setColour(Label::textColourId, Colours::grey);
+    label.setJustificationType(Justification::centredBottom);
 }
--- DeckGUI.h
+++ DeckGUI.h
@@ -8,62 +8,133 @@
 #include "TrackInfoDisplay.h"
 #include "DeckButton.h"
 
-class DeckGUI : public juce::Component,
-                public juce::Button::Listener,
-                public juce::Slider::Listener,
-                public juce::FileDragAndDropTarget,
+/**
+ * A class to represent the graphical user interface of a DJ deck
+ */
+class DeckGUI : public Component,
+                public Button::Listener,
+                public Slider::Listener,
+                public FileDragAndDropTarget,
                 public DragAndDropTarget,
-                public juce::Timer
-{
+                public Timer {
 public:
-    explicit DeckGUI(DJAudioPlayer* player,
-                     AudioFormatManager &formatManagerToUse,
-                     AudioThumbnailCache &cacheToUse);
+    DeckGUI(DJAudioPlayer *player,
+            AudioFormatManager &formatManagerToUse,
+            AudioThumbnailCache &cacheToUse);
+
     ~DeckGUI() override;
 
-    void loadFile(File& file);
+    void loadFile(File &file);
 
     //================ Component pure virtual functions ================
-    void paint(Graphics& g) override;
+
+    /**
+     * Components can override this method to draw their content
+     * @param g the graphics context that must be used to do the drawing operations
+     */
+    void paint(Graphics &g) override;
+
+    /**
+     * Called when this component's size has been changed
+     */
     void resized() override;
 
     //================ Button pure virtual functions ================
-    void buttonClicked(Button *) override;
+
+    /**
+     * Called when the button is clicked
+     * @param button Button pointer
+     */
+    void buttonClicked(Button *button) override;
 
     //================ Slider pure virtual functions ================
+
+    /**
+     * Called when the slider's value is changed
+     * @param slider Slider pointer
+     */
     void sliderValueChanged(Slider *slider) override;
 
     //================ FileDragAndDropTarget pure virtual functions ================
+
+    /**
+     * Callback to check whether this target is interested in the set of files being offered
+     * @param files the set of (absolute) pathnames of the files that the user is dragging
+     * @return true if this component wants to receive the other callbacks regarding this type of object;
+     * if it returns false, no other callbacks will be made
+     */
     bool isInterestedInFileDrag(const StringArray &files) override;
+
+    /**
+     * Callback to indicate that the user has dropped the files onto this component
+     * @param files the set of (absolute) pathnames of the files that the user is dragging
+     * @param x the mouse x position, relative to this component
+     * @param y the mouse y position, relative to this component
+     */
     void filesDropped(const StringArray &files, int x, int y) override;
 
     //================ DragAndDropTarget pure virtual functions ================
+
+    /**
+     * Callback to check whether this target is interested in the type of object being dragged
+     * @param dragSourceDetails contains information about the source of the drag operation
+     * @return true if this component wants to receive the other callbacks regarding this type of object;
+     * if it returns false, no other callbacks will be made
+     */
     bool isInterestedInDragSource(const SourceDetails &dragSourceDetails) override;
+
+    /**
+     * Callback to indicate that the user has dropped something onto this component
+     * @param dragSourceDetails contains information about the source of the drag operation
+     */
     void itemDropped(const SourceDetails &dragSourceDetails) override;
 
     //================ Timer pure virtual functions ================
+
+    /**
+     * The user-defined callback routine that actually gets called periodically
+     */
     void timerCallback() override;
+
 private:
 
-    void formatLabel(Label& label, std::string text);
+    /**
+     * sets the correct formatting for the labels on the deck
+     * @param label Label instance to be formatted
+     * @param text Text to be set on the label
+     */
+    void formatLabel(Label &label, std::string text);
 
+    // custom deck buttons to control playback of audio on DJ deck
     DeckButton playPauseButton{"", Colour::fromString("#FF00CC00"), true};
     DeckButton cueButton{"CUE", Colour::fromString("#FFE67300")};
 
-    juce::Slider tempoSlider{Slider::SliderStyle::LinearVertical, Slider::TextEntryBoxPosition::NoTextBox};
+    // slider to control the tempo of the track being played on the deck
+    Slider tempoSlider{Slider::SliderStyle::LinearVertical, Slider::TextEntryBoxPosition::NoTextBox};
+
+    // label for the tempo slider
     Label tempoLabel;
+
+    // shared pointer for the tempo value of the audio on the deck
     std::shared_ptr<double> tempoVal = std::make_shared<double>(1.0);
 
-    DJAudioPlayer* player;
+    // pointer to the instance of the DJAudioPlayer handling audio for the DJ deck
+    DJAudioPlayer *player;
 
+    // Component to quickly control the tempo of the track, aids the DJ in beat matching
     Jogwheel jogWheel{player, tempoVal};
 
-    juce::FileChooser fChooser{"Select a file..."};
+    // Creates a dialog box to choose a file or directory to load or save
+    FileChooser fChooser{"Select a file..."};
 
+    // Component to display the information of the current track on the deck
     TrackInfoDisplay trackInfoDisplay;
+
+    // Component to display the Waveform of the current track on the deck
     WaveformDisplay waveformDisplay;
 
-    int TIMER_INTERVAL = 100;
+    // constant for the rate the timer callback fires
+    const int TIMER_INTERVAL = 100;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(DeckGUI)
 };
