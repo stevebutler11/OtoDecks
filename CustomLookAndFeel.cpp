b5d9c99adb0a0e0cf761880f9fc52c09d1ce7c4e
steve <stevebutler11@gmail.com>
2023-03-10 10:55:17 +0000

    CustomLookAndFeel tidied

--- CustomLookAndFeel.cpp
+++ CustomLookAndFeel.cpp
@@ -1,36 +1,15 @@
 #include "CustomLookAndFeel.h"
 
-CustomLookAndFeel::CustomLookAndFeel()
-{
-//    // midnight colour scheme
-//    this->setColourScheme(LookAndFeel_V4::ColourScheme({
-//        // windowBackground
-//        0xff2f2f3a,
-//        // widgetBackground
-//        0xff191926,
-//        // menuBackground
-//        0xffd0d0d0,
-//        // outline
-//        0xff66667c,
-//        // defaultText
-//        0xc8ffffff,
-//        // defaultFill
-//        0xffd8d8d8,
-//        // highlightedText
-//        0xffffffff,
-//        // highlightedFill
-//        0xff606073,
-//        // menuText
-//        0xff000000
-//    }));
+CustomLookAndFeel::CustomLookAndFeel() {
 
+    // set some default colours for the colour scheme
     this->setColourScheme(LookAndFeel_V4::ColourScheme({
         // windowBackground
         0xff3C3F41,
         // widgetBackground
         0xff2B2B2B,
         // menuBackground
-        0xffd0d0d0, // unchanged
+        0xffd0d0d0,
         // outline
         0xff6793B3,
         // defaultText
@@ -46,78 +25,78 @@ CustomLookAndFeel::CustomLookAndFeel()
     }));
 }
 
-CustomLookAndFeel::~CustomLookAndFeel()
-{
+CustomLookAndFeel::~CustomLookAndFeel() {
 
 }
 
-void CustomLookAndFeel::drawLinearSlider(Graphics& g, int x, int y, int width, int height, float sliderPos, float minSliderPos,
-                       float maxSliderPos, const Slider::SliderStyle style, Slider& slider)
-{
-    if (slider.isBar())
-    {
-        g.setColour (slider.findColour (Slider::trackColourId));
-        g.fillRect (slider.isHorizontal() ? Rectangle<float> (static_cast<float> (x), (float) y + 0.5f, sliderPos - (float) x, (float) height - 1.0f)
-                                          : Rectangle<float> ((float) x + 0.5f, sliderPos, (float) width - 1.0f, (float) y + ((float) height - sliderPos)));
-    }
-    else
-    {
-        auto isTwoVal   = (style == Slider::SliderStyle::TwoValueVertical   || style == Slider::SliderStyle::TwoValueHorizontal);
-        auto isThreeVal = (style == Slider::SliderStyle::ThreeValueVertical || style == Slider::SliderStyle::ThreeValueHorizontal);
+void CustomLookAndFeel::drawLinearSlider(Graphics &g, int x, int y, int width, int height, float sliderPos,
+                                         float minSliderPos,
+                                         float maxSliderPos, const Slider::SliderStyle style, Slider &slider) {
 
-        auto trackWidth = jmin (6.0f, slider.isHorizontal() ? (float) height * 0.25f : (float) width * 0.25f);
+    /**
+     * N.B. the code below is built upon from the code provided in the drawLinearSlider function itself
+     * */
 
-        Point<float> startPoint (slider.isHorizontal() ? (float) x : (float) x + (float) width * 0.5f,
-                                 slider.isHorizontal() ? (float) y + (float) height * 0.5f : (float) (height + y));
+    if (slider.isBar()) {
+        g.setColour(slider.findColour(Slider::trackColourId));
+        g.fillRect(slider.isHorizontal() ? Rectangle<float>(static_cast<float> (x), (float) y + 0.5f,
+                                                            sliderPos - (float) x, (float) height - 1.0f)
+                                         : Rectangle<float>((float) x + 0.5f, sliderPos, (float) width - 1.0f,
+                                                            (float) y + ((float) height - sliderPos)));
+    } else {
+        auto isTwoVal = (style == Slider::SliderStyle::TwoValueVertical ||
+                         style == Slider::SliderStyle::TwoValueHorizontal);
+        auto isThreeVal = (style == Slider::SliderStyle::ThreeValueVertical ||
+                           style == Slider::SliderStyle::ThreeValueHorizontal);
 
-        Point<float> endPoint (slider.isHorizontal() ? (float) (width + x) : startPoint.x,
-                               slider.isHorizontal() ? startPoint.y : (float) y);
+        auto trackWidth = jmin(6.0f, slider.isHorizontal() ? (float) height * 0.25f : (float) width * 0.25f);
+
+        Point<float> startPoint(slider.isHorizontal() ? (float) x : (float) x + (float) width * 0.5f,
+                                slider.isHorizontal() ? (float) y + (float) height * 0.5f : (float) (height + y));
+
+        Point<float> endPoint(slider.isHorizontal() ? (float) (width + x) : startPoint.x,
+                              slider.isHorizontal() ? startPoint.y : (float) y);
 
         Path backgroundTrack;
-        backgroundTrack.startNewSubPath (startPoint);
-        backgroundTrack.lineTo (endPoint);
-        g.setColour (slider.findColour (Slider::backgroundColourId));
-        g.strokePath (backgroundTrack, { trackWidth, PathStrokeType::curved, PathStrokeType::square });
+        backgroundTrack.startNewSubPath(startPoint);
+        backgroundTrack.lineTo(endPoint);
+        g.setColour(slider.findColour(Slider::backgroundColourId));
+        g.strokePath(backgroundTrack, {trackWidth, PathStrokeType::curved, PathStrokeType::square});
 
         Path valueTrack;
         Point<float> minPoint, maxPoint, thumbPoint;
 
-        if (isTwoVal || isThreeVal)
-        {
-            minPoint = { slider.isHorizontal() ? minSliderPos : (float) width * 0.5f,
-                         slider.isHorizontal() ? (float) height * 0.5f : minSliderPos };
+        if (isTwoVal || isThreeVal) {
+            minPoint = {slider.isHorizontal() ? minSliderPos : (float) width * 0.5f,
+                        slider.isHorizontal() ? (float) height * 0.5f : minSliderPos};
 
             if (isThreeVal)
-                thumbPoint = { slider.isHorizontal() ? sliderPos : (float) width * 0.5f,
-                               slider.isHorizontal() ? (float) height * 0.5f : sliderPos };
+                thumbPoint = {slider.isHorizontal() ? sliderPos : (float) width * 0.5f,
+                              slider.isHorizontal() ? (float) height * 0.5f : sliderPos};
 
-            maxPoint = { slider.isHorizontal() ? maxSliderPos : (float) width * 0.5f,
-                         slider.isHorizontal() ? (float) height * 0.5f : maxSliderPos };
-        }
-        else
-        {
+            maxPoint = {slider.isHorizontal() ? maxSliderPos : (float) width * 0.5f,
+                        slider.isHorizontal() ? (float) height * 0.5f : maxSliderPos};
+        } else {
             auto kx = slider.isHorizontal() ? sliderPos : ((float) x + (float) width * 0.5f);
             auto ky = slider.isHorizontal() ? ((float) y + (float) height * 0.5f) : sliderPos;
 
             minPoint = startPoint;
-            maxPoint = { kx, ky };
+            maxPoint = {kx, ky};
         }
 
-        auto thumbWidth = getSliderThumbRadius (slider);
+        auto thumbWidth = getSliderThumbRadius(slider);
 
-        valueTrack.startNewSubPath (minPoint);
-        valueTrack.lineTo (isThreeVal ? thumbPoint : maxPoint);
-        g.setColour (slider.findColour (Slider::trackColourId));
-        g.strokePath (valueTrack, { trackWidth, PathStrokeType::curved, PathStrokeType::square });
+        valueTrack.startNewSubPath(minPoint);
+        valueTrack.lineTo(isThreeVal ? thumbPoint : maxPoint);
+        g.setColour(slider.findColour(Slider::trackColourId));
+        g.strokePath(valueTrack, {trackWidth, PathStrokeType::curved, PathStrokeType::square});
 
-        if (! isTwoVal)
-        {
-            if (style == Slider::SliderStyle::LinearVertical)
-            {
-                auto thumbRect = Rectangle<float> (
-                static_cast<float> (thumbWidth * 3),
+        if (!isTwoVal) {
+            if (style == Slider::SliderStyle::LinearVertical) {
+                auto thumbRect = Rectangle<float>(
+                        static_cast<float> (thumbWidth * 3),
                         static_cast<float> (thumbWidth * 2)
-                ).withCentre (isThreeVal ? thumbPoint : maxPoint);
+                ).withCentre(isThreeVal ? thumbPoint : maxPoint);
 
                 auto cg = ColourGradient(Colour::fromString("#FF333333"),
                                          thumbRect.getX(), thumbRect.getY(),
@@ -139,12 +118,11 @@ void CustomLookAndFeel::drawLinearSlider(Graphics& g, int x, int y, int width, i
                         thumbRect.getCentreY(),
                         1);
             }
-            if (style == Slider::SliderStyle::LinearHorizontal)
-            {
-                auto thumbRect = Rectangle<float> (
+            if (style == Slider::SliderStyle::LinearHorizontal) {
+                auto thumbRect = Rectangle<float>(
                         static_cast<float> (thumbWidth * 2),
                         static_cast<float> (thumbWidth * 3)
-                ).withCentre (isThreeVal ? thumbPoint : maxPoint);
+                ).withCentre(isThreeVal ? thumbPoint : maxPoint);
 
                 auto cg = ColourGradient(Colour::fromString("#FF333333"),
                                          thumbRect.getX(), thumbRect.getY(),
@@ -168,29 +146,26 @@ void CustomLookAndFeel::drawLinearSlider(Graphics& g, int x, int y, int width, i
             }
         }
 
-        if (isTwoVal || isThreeVal)
-        {
-            auto sr = jmin (trackWidth, (slider.isHorizontal() ? (float) height : (float) width) * 0.4f);
-            auto pointerColour = slider.findColour (Slider::thumbColourId);
+        if (isTwoVal || isThreeVal) {
+            auto sr = jmin(trackWidth, (slider.isHorizontal() ? (float) height : (float) width) * 0.4f);
+            auto pointerColour = slider.findColour(Slider::thumbColourId);
 
-            if (slider.isHorizontal())
-            {
-                drawPointer (g, minSliderPos - sr,
-                             jmax (0.0f, (float) y + (float) height * 0.5f - trackWidth * 2.0f),
-                             trackWidth * 2.0f, pointerColour, 2);
+            if (slider.isHorizontal()) {
+                drawPointer(g, minSliderPos - sr,
+                            jmax(0.0f, (float) y + (float) height * 0.5f - trackWidth * 2.0f),
+                            trackWidth * 2.0f, pointerColour, 2);
 
-                drawPointer (g, maxSliderPos - trackWidth,
-                             jmin ((float) (y + height) - trackWidth * 2.0f, (float) y + (float) height * 0.5f),
-                             trackWidth * 2.0f, pointerColour, 4);
-            }
-            else
-            {
-                drawPointer (g, jmax (0.0f, (float) x + (float) width * 0.5f - trackWidth * 2.0f),
-                             minSliderPos - trackWidth,
-                             trackWidth * 2.0f, pointerColour, 1);
+                drawPointer(g, maxSliderPos - trackWidth,
+                            jmin((float) (y + height) - trackWidth * 2.0f, (float) y + (float) height * 0.5f),
+                            trackWidth * 2.0f, pointerColour, 4);
+            } else {
+                drawPointer(g, jmax(0.0f, (float) x + (float) width * 0.5f - trackWidth * 2.0f),
+                            minSliderPos - trackWidth,
+                            trackWidth * 2.0f, pointerColour, 1);
 
-                drawPointer (g, jmin ((float) (x + width) - trackWidth * 2.0f, (float) x + (float) width * 0.5f), maxSliderPos - sr,
-                             trackWidth * 2.0f, pointerColour, 3);
+                drawPointer(g, jmin((float) (x + width) - trackWidth * 2.0f, (float) x + (float) width * 0.5f),
+                            maxSliderPos - sr,
+                            trackWidth * 2.0f, pointerColour, 3);
             }
         }
     }
--- CustomLookAndFeel.h
+++ CustomLookAndFeel.h
@@ -3,15 +3,40 @@
 
 #include <JuceHeader.h>
 
-class CustomLookAndFeel : public LookAndFeel_V4
-{
+/**
+ * Overrides the LookAndFeel_V4 class to create a custom theme for the application
+ */
+class CustomLookAndFeel : public LookAndFeel_V4 {
 public:
+
+    /**
+     * CustomLookAndFeel constructor
+     */
     CustomLookAndFeel();
+
+    /**
+     * CustomLookAndFeel destructor
+     */
     ~CustomLookAndFeel() override;
 
     //================ LookAndFeel_V4 override functions ================
-    void drawLinearSlider (Graphics &, int x, int y, int width, int height, float sliderPos, float minSliderPos,
-                           float maxSliderPos, const Slider::SliderStyle, Slider &) override;
+
+    /**
+     * Overrides the function to draw linear sliders
+     * @param x x pos of linear slider
+     * @param y y pos of linear slider
+     * @param width width of linear slider
+     * @param height height of linear slider
+     * @param sliderPos starting position of the slider
+     * @param minSliderPos lowest value of slider
+     * @param maxSliderPos highest value of slider
+     * @param style style of the slider
+     * @param slider reference to the slider itself
+     */
+    void drawLinearSlider(Graphics &, int x, int y, int width, int height, float sliderPos, float minSliderPos,
+                          float maxSliderPos, const Slider::SliderStyle style, Slider &slider) override;
+
 private:
 };
+
 #endif //GUI_APP_EXAMPLE_CUSTOMLOOKANDFEEL_H
