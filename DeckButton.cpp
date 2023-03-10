84eba437529d0b16aa64d8085f2f7d5cee5d5b84
steve <stevebutler11@gmail.com>
2023-03-10 11:05:35 +0000

    DeckButton tidied

--- DeckButton.cpp
+++ DeckButton.cpp
@@ -1,82 +1,77 @@
 #include "DeckButton.h"
 
-DeckButton::DeckButton(const std::string& buttonName, const Colour& _outerGlowColour, bool _isPlayButton)
-                        : Button(buttonName), outerGlowColour(_outerGlowColour), isPlayButton(_isPlayButton)
-{
+DeckButton::DeckButton(const std::string &buttonName, const Colour &_outerGlowColour, bool _isPlayButton)
+        : Button(buttonName), outerGlowColour(_outerGlowColour), isPlayButton(_isPlayButton) {
 
 }
 
-void DeckButton::paintButton(juce::Graphics& g, bool isMouseOver, bool isButtonDown)
-{
+void DeckButton::paintButton(juce::Graphics &g, bool isMouseOver, bool isButtonDown) {
     auto buttonColour = getLookAndFeel().findColour(juce::TextButton::buttonColourId);
     auto buttonBounds = getLocalBounds().toFloat().reduced(1.0f);
     auto diameter = buttonBounds.getHeight() > buttonBounds.getWidth() ?
                     buttonBounds.getWidth() : buttonBounds.getHeight();
     g.setColour(buttonColour);
     g.fillEllipse(
-            buttonBounds.getCentreX() - diameter/2,
-            buttonBounds.getCentreY() - diameter/2,
+            buttonBounds.getCentreX() - diameter / 2,
+            buttonBounds.getCentreY() - diameter / 2,
             diameter,
             diameter);
 
     // scale font with button size
-    g.setFont(diameter/4);
+    g.setFont(diameter / 4);
 
-    if (isMouseOver || isButtonDown)
-    {
+    if (isMouseOver || isButtonDown) {
         g.setColour(outerGlowColour);
         g.drawEllipse(
-                buttonBounds.getCentreX() - diameter/2,
-                buttonBounds.getCentreY() - diameter/2,
+                buttonBounds.getCentreX() - diameter / 2,
+                buttonBounds.getCentreY() - diameter / 2,
                 diameter,
                 diameter,
                 1.0f);
         g.setColour(findColour(TextButton::ColourIds::textColourOnId));
         if (isPlayButton) {
             Path playSymbol;
-            playSymbol.addTriangle (
-                    buttonBounds.getCentreX() - diameter/8, buttonBounds.getCentreY() - diameter/12,
+            playSymbol.addTriangle(
+                    buttonBounds.getCentreX() - diameter / 8, buttonBounds.getCentreY() - diameter / 12,
                     buttonBounds.getCentreX(), buttonBounds.getCentreY(),
-                    buttonBounds.getCentreX() - diameter/8, buttonBounds.getCentreY() + diameter/12);
-            g.fillPath (playSymbol);
+                    buttonBounds.getCentreX() - diameter / 8, buttonBounds.getCentreY() + diameter / 12);
+            g.fillPath(playSymbol);
 
             g.fillRect(Rectangle<float>(
                     buttonBounds.getCentreX(),
-                    buttonBounds.getCentreY() - diameter/12,
+                    buttonBounds.getCentreY() - diameter / 12,
                     diameter / 24,
-                    diameter/6));
+                    diameter / 6));
 
             g.fillRect(Rectangle<float>(
-                    buttonBounds.getCentreX() + diameter/12,
-                    buttonBounds.getCentreY() - diameter/12,
+                    buttonBounds.getCentreX() + diameter / 12,
+                    buttonBounds.getCentreY() - diameter / 12,
                     diameter / 24,
-                    diameter/6));
+                    diameter / 6));
         } else {
             g.drawText(this->getButtonText(), buttonBounds, Justification::centred);
         }
-    }
-    else
-    {
+    } else {
         g.setColour(findColour(TextButton::ColourIds::textColourOffId));
         if (isPlayButton) {
             Path playSymbol;
-            playSymbol.addTriangle (
-                    buttonBounds.getCentreX() - diameter/8, buttonBounds.getCentreY() - diameter/12,
+            playSymbol.addTriangle(
+                    buttonBounds.getCentreX() - diameter / 8, buttonBounds.getCentreY() - diameter / 12,
                     buttonBounds.getCentreX(), buttonBounds.getCentreY(),
-                    buttonBounds.getCentreX() - diameter/8, buttonBounds.getCentreY() + diameter/12);
-            g.fillPath (playSymbol);
+                    buttonBounds.getCentreX() - diameter / 8, buttonBounds.getCentreY() + diameter / 12);
+            g.fillPath(playSymbol);
 
             g.fillRect(Rectangle<float>(
                     buttonBounds.getCentreX(),
-                    buttonBounds.getCentreY() - diameter/12,
+                    buttonBounds.getCentreY() - diameter / 12,
                     diameter / 24,
-                    diameter/6));
+                    diameter / 6));
 
             g.fillRect(Rectangle<float>(
-                    buttonBounds.getCentreX() + diameter/12,
-                    buttonBounds.getCentreY() - diameter/12,
+                    buttonBounds.getCentreX() + diameter / 12,
+                    buttonBounds.getCentreY() - diameter / 12,
                     diameter / 24,
-                    diameter/6));
+                    diameter / 6));
 
         } else {
             g.drawText(this->getButtonText(), buttonBounds, Justification::centred);
--- DeckButton.h
+++ DeckButton.h
@@ -3,15 +3,41 @@
 
 #include <JuceHeader.h>
 
-class DeckButton : public Button
-{
+/**
+ * Overrides the JUCE button class to create a button more suited to the DJ Deck
+ */
+class DeckButton : public Button {
 public:
-    DeckButton(const std::string& buttonName, const Colour& outerGlowColour, bool isPlayButton=false);
+
+    /**
+     * DeckButton constructor
+     * @param buttonName the text of the button
+     * @param outerGlowColour JUCE colour to set the outer glow to
+     * @param isPlayButton boolean representing if this button is of the 'play/pause' variety
+     */
+    DeckButton(const std::string &buttonName, const Colour &outerGlowColour, bool isPlayButton = false);
+
+    /**
+     * DeckButton destructor
+     */
     ~DeckButton() override = default;
 
 private:
-    void paintButton(juce::Graphics& g, bool isMouseOver, bool isButtonDown) override;
+
+    //================ Button override functions ================
+
+    /**
+     * Override funtion that paints the button's contents
+     * @param g reference to the JUCE Graphics instance
+     * @param isMouseOver boolean representing whether the mouse is hovering over the button
+     * @param isButtonDown boolean representing whether the button is down
+     */
+    void paintButton(juce::Graphics &g, bool isMouseOver, bool isButtonDown) override;
+
+    // JUCE Colour to set the outer glow to
     Colour outerGlowColour;
+
+    // boolean representing if this button is of the 'play/pause' variety
     bool isPlayButton;
 };
 
