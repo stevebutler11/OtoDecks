d0a1cdaffc6d60dcd5425c678d37864b74e89377
steve <stevebutler11@gmail.com>
2023-03-10 14:13:12 +0000

    TwoChannelMixer tidied

--- TwoChannelMixer.cpp
+++ TwoChannelMixer.cpp
@@ -1,8 +1,7 @@
 #include "TwoChannelMixer.h"
 
-TwoChannelMixer::TwoChannelMixer(DJAudioPlayer* _leftPlayer, DJAudioPlayer* _rightPlayer)
-                                : leftPlayer(_leftPlayer), rightPlayer(_rightPlayer)
-{
+TwoChannelMixer::TwoChannelMixer(DJAudioPlayer *_leftPlayer, DJAudioPlayer *_rightPlayer)
+        : leftPlayer(_leftPlayer), rightPlayer(_rightPlayer) {
     addAndMakeVisible(leftDeckVolume);
     addAndMakeVisible(rightDeckVolume);
     addAndMakeVisible(crossfader);
@@ -27,84 +26,61 @@ TwoChannelMixer::TwoChannelMixer(DJAudioPlayer* _leftPlayer, DJAudioPlayer* _rig
     formatLabel(crossfaderLabel, "CROSSFADER");
 }
 
-TwoChannelMixer::~TwoChannelMixer()
-{
+TwoChannelMixer::~TwoChannelMixer() {}
 
-}
-
-void TwoChannelMixer::paint(Graphics& g)
-{
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
-    g.setColour(getLookAndFeel().findColour (juce::ListBox::ColourIds::backgroundColourId));
+void TwoChannelMixer::paint(Graphics &g) {
+    g.fillAll(getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
+    g.setColour(getLookAndFeel().findColour(juce::ListBox::ColourIds::backgroundColourId));
     g.drawRoundedRectangle(getLocalBounds().toFloat(), 5.0f, 1.0f);
 }
 
-void TwoChannelMixer::resized()
-{
+void TwoChannelMixer::resized() {
     auto widthSlice = getBounds().getWidth() / 8;
     auto heightSlice = getBounds().getHeight() / 7;
 
     leftDeckVolume.setBounds(0, heightSlice / 2, widthSlice * 3, heightSlice * 5);
-    leftVolLabel.setBounds(0, (int) (heightSlice * 5.5), widthSlice * 3, heightSlice/4);
+    leftVolLabel.setBounds(0, (int) (heightSlice * 5.5), widthSlice * 3, heightSlice / 4);
 
     rightDeckVolume.setBounds(widthSlice * 5, heightSlice / 2, widthSlice * 3, heightSlice * 5);
-    rightVolLabel.setBounds(widthSlice * 5, (int) (heightSlice * 5.5), widthSlice * 3, heightSlice/4);
+    rightVolLabel.setBounds(widthSlice * 5, (int) (heightSlice * 5.5), widthSlice * 3, heightSlice / 4);
     crossfader.setBounds(widthSlice, (int) (heightSlice * 6), widthSlice * 6, heightSlice);
-    crossfaderLabel.setBounds(widthSlice, (int) (heightSlice * 6.5), widthSlice * 6, heightSlice/2);
+    crossfaderLabel.setBounds(widthSlice, (int) (heightSlice * 6.5), widthSlice * 6, heightSlice / 2);
 }
 
-void TwoChannelMixer::sliderValueChanged(Slider *slider)
-{
+void TwoChannelMixer::sliderValueChanged(Slider *slider) {
     auto val = crossfader.getValue();
 
-    if (slider == &leftDeckVolume)
-    {
-        if (val <= 0)
-        {
+    if (slider == &leftDeckVolume) {
+        if (val <= 0) {
             leftPlayer->setGain(leftDeckVolume.getValue());
-        }
-        else
-        {
+        } else {
             leftPlayer->setGain(leftDeckVolume.getValue() * (1 - val));
         }
     }
 
-    if (slider == &rightDeckVolume)
-    {
-        if (val <= 0)
-        {
+    if (slider == &rightDeckVolume) {
+        if (val <= 0) {
             rightPlayer->setGain(rightDeckVolume.getValue() * (1 + val));
-        }
-        else
-        {
+        } else {
             rightPlayer->setGain(rightDeckVolume.getValue());
         }
     }
 
-    if (slider == &crossfader)
-    {
-        if (val <= 0)
-        {
-            // set right deck to rightDeckVolume value
+    if (slider == &crossfader) {
+        if (val <= 0) {
             rightPlayer->setGain(rightDeckVolume.getValue() * (1 + val));
-            // set left deck to leftDeckVolume * 1 - crossFader.getValue()
             leftPlayer->setGain(leftDeckVolume.getValue());
-        }
-        else
-        {
-            // set left deck to leftDeckVolume value
+        } else {
             leftPlayer->setGain(leftDeckVolume.getValue() * (1 - val));
-            // set right deck to rightDeckVolume * crossFader.getValue()
             rightPlayer->setGain(rightDeckVolume.getValue());
         }
     }
 }
 
-void TwoChannelMixer::formatLabel(Label& label, std::string text)
-{
+void TwoChannelMixer::formatLabel(Label &label, std::string text) {
     Typeface::Ptr tface = Typeface::createSystemTypefaceFor(BinaryData::neuropol_otf, BinaryData::neuropol_otfSize);
-    label.setText (text, juce::dontSendNotification);
+    label.setText(text, juce::dontSendNotification);
     label.setFont(Font(tface).withHeight(10.0f));
-    label.setColour (juce::Label::textColourId, juce::Colours::grey);
-    label.setJustificationType (juce::Justification::centredBottom);
+    label.setColour(juce::Label::textColourId, juce::Colours::grey);
+    label.setJustificationType(juce::Justification::centredBottom);
 }
--- TwoChannelMixer.h
+++ TwoChannelMixer.h
@@ -4,30 +4,73 @@
 #include <JuceHeader.h>
 #include "DJAudioPlayer.h"
 
+/**
+ * A class that represents a two channel mixer on a DJ deck.
+ * This will be used control the volume of the two 'channels' of the DJ setup and mix the two audio sources together
+ */
 class TwoChannelMixer : public Component,
-                        public Slider::Listener
-{
+                        public Slider::Listener {
 public:
-    TwoChannelMixer(DJAudioPlayer* leftPlayer, DJAudioPlayer* rightPlayer);
+
+    /**
+     * TwoChannelMixer constructor
+     * @param leftPlayer a pointer to the left deck's DJAudioPlayer instance
+     * @param rightPlayer a pointer to the right deck's DJAudioPlayer instance
+     */
+    TwoChannelMixer(DJAudioPlayer *leftPlayer, DJAudioPlayer *rightPlayer);
+
+    /**
+     * TwoChannelMixer destructor
+     */
     ~TwoChannelMixer() override;
 
     //================ Component pure virtual functions ================
-    void paint(Graphics& g) override;
+
+    /**
+     * Components can override this method to draw their content
+     * @param g the graphics context that must be used to do the drawing operations
+     */
+    void paint(Graphics &g) override;
+
+    /**
+     * Called when this component's size has been changed
+     */
     void resized() override;
 
-    //================ Slider pure virtual functions ================
+    //================ Slider:Listener pure virtual functions ================
+
+    /**
+     * Called when the slider's value is changed
+     * @param slider Slider pointer
+     */
     void sliderValueChanged(Slider *slider) override;
 
 private:
 
-    static void formatLabel(Label& label, std::string text);
+    /**
+     * Applies the correct styling to the label for the channel mixer component
+     * @param label the label to be styled
+     * @param text the text to add to the label
+     */
+    static void formatLabel(Label &label, std::string text);
 
-    DJAudioPlayer* leftPlayer;
-    DJAudioPlayer* rightPlayer;
+    // a pointer to the left deck's DJAudioPlayer instance
+    DJAudioPlayer *leftPlayer;
+
+    // a pointer to the right deck's DJAudioPlayer instance
+    DJAudioPlayer *rightPlayer;
+
+    // slider for the left deck volume
     Slider leftDeckVolume{Slider::SliderStyle::LinearVertical, Slider::TextEntryBoxPosition::NoTextBox};
+
+    // slider for the right deck volume
     Slider rightDeckVolume{Slider::SliderStyle::LinearVertical, Slider::TextEntryBoxPosition::NoTextBox};
+
+    // slider for the deck crossfader
     Slider crossfader{Slider::SliderStyle::LinearHorizontal, Slider::TextEntryBoxPosition::NoTextBox};
-    Label leftVolLabel, rightVolLabel;
-    Label crossfaderLabel;
+
+    // Labels for the sliders
+    Label leftVolLabel, rightVolLabel, crossfaderLabel;
 };
+
 #endif //GUI_APP_EXAMPLE_TWOCHANNELMIXER_H
