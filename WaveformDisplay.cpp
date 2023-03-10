6125c1a70b60d1de8e97e382f0ca41c0dc6cde8b
steve <stevebutler11@gmail.com>
2023-03-10 14:40:53 +0000

    WaveformDisplay tidied

--- WaveformDisplay.cpp
+++ WaveformDisplay.cpp
@@ -1,32 +1,27 @@
 #include "WaveformDisplay.h"
 
-WaveformDisplay::WaveformDisplay(DJAudioPlayer* _player,
+WaveformDisplay::WaveformDisplay(DJAudioPlayer *_player,
                                  AudioFormatManager &formatManagerToUse,
                                  AudioThumbnailCache &cacheToUse
-                                 ) : player(_player), audioThumbnail(
-                                         1000,
-                                         formatManagerToUse,
-                                         cacheToUse), fileLoaded(false), position(0.0)
-{
+) : player(_player), audioThumbnail(
+        1000,
+        formatManagerToUse,
+        cacheToUse), fileLoaded(false), position(0.0) {
     audioThumbnail.addChangeListener(this);
 }
-WaveformDisplay::~WaveformDisplay()
-{
 
-}
+WaveformDisplay::~WaveformDisplay() {}
 
-void WaveformDisplay::paint(Graphics& g)
-{
-    g.fillAll (getLookAndFeel().findColour(ListBox::ColourIds::backgroundColourId));
+void WaveformDisplay::paint(Graphics &g) {
+    g.fillAll(getLookAndFeel().findColour(ListBox::ColourIds::backgroundColourId));
 
-    if (fileLoaded)
-    {
+    if (fileLoaded) {
         auto cg = ColourGradient(Colour::fromString("FFC75450"),
-                       0.0, 0.0,
-                       Colour::fromString("FFC75450"),
-                       0.0,
-                       getLocalBounds().getHeight(),
-                       false);
+                                 0.0, 0.0,
+                                 Colour::fromString("FFC75450"),
+                                 0.0,
+                                 getLocalBounds().getHeight(),
+                                 false);
 
         cg.addColour(0.5, Colour::fromString("FF3592C4"));
         g.setGradientFill(cg);
@@ -58,8 +53,7 @@ void WaveformDisplay::paint(Graphics& g)
         g.fillRect(cueMap, 0, 1, getLocalBounds().getHeight());
 
         // draw current position
-        if (!std::isnan(position))
-        {
+        if (!std::isnan(position)) {
             g.setColour(juce::Colours::lightgreen);
 
             auto posMap = WaveformDisplay::map(
@@ -73,67 +67,51 @@ void WaveformDisplay::paint(Graphics& g)
         }
     }
 }
-void WaveformDisplay::resized()
-{
+
+void WaveformDisplay::resized() {
 
 }
 
-void WaveformDisplay::loadURL(const URL& audioURL)
-{
+void WaveformDisplay::loadURL(const URL &audioURL) {
     audioThumbnail.clear();
     fileLoaded = audioThumbnail.setSource(new URLInputSource(audioURL));
-    //TODO: change to throw error and catch in deckGUI?
-    if (fileLoaded)
-    {
-        std::cout << "WaveformDisplay::loadURL loaded successfully" << std::endl;
-    }
-    else
-    {
+    if (!fileLoaded) {
         std::cout << "WaveformDisplay::loadURL load error" << std::endl;
     }
 }
 
-void WaveformDisplay::setPositionRelative(double pos)
-{
-    if (position != pos)
-    {
+void WaveformDisplay::setPositionRelative(double pos) {
+    if (position != pos) {
         position = pos;
         repaint();
     }
 }
 
-double WaveformDisplay::getCuePositionRelative() const
-{
+double WaveformDisplay::getCuePositionRelative() const {
     return cuePosition;
 }
 
-void WaveformDisplay::changeListenerCallback(ChangeBroadcaster *source)
-{
+void WaveformDisplay::changeListenerCallback(ChangeBroadcaster *source) {
     repaint();
 }
 
-void WaveformDisplay::mouseDown(const MouseEvent &event)
-{
+void WaveformDisplay::mouseDown(const MouseEvent &event) {
     auto trackWidthRatio = getTrackWidthRatio();
 
     // setting cue position
-    if (event.mods.isShiftDown())
-    {
+    if (event.mods.isShiftDown()) {
         cuePosition = trackWidthRatio;
         repaint();
     }
-    // setting play position
-    else
-    {
+        // setting play position
+    else {
         player->setPositionRelative(trackWidthRatio);
     }
 }
 
-void WaveformDisplay::mouseWheelMove(const MouseEvent& event, const MouseWheelDetails& wheel)
-{
-    if (event.mods.isShiftDown())
-    {
-        zoomFactor = (zoomFactor + (wheel.deltaY*80) > 1) ? zoomFactor + (wheel.deltaY*80) : 1.0;
+void WaveformDisplay::mouseWheelMove(const MouseEvent &event, const MouseWheelDetails &wheel) {
+    if (event.mods.isShiftDown()) {
+        zoomFactor = (zoomFactor + (wheel.deltaY * 80) > 1) ? zoomFactor + (wheel.deltaY * 80) : 1.0;
         auto mouseWidthRatio = getMouseWidthRatio();
         leftWidthOffset = mouseWidthRatio;
         rightWidthOffset = 1 - mouseWidthRatio;
@@ -141,21 +119,18 @@ void WaveformDisplay::mouseWheelMove(const MouseEvent& event, const MouseWheelDe
     }
 }
 
-double WaveformDisplay::getMouseWidthRatio()
-{
+double WaveformDisplay::getMouseWidthRatio() {
     auto mouseXY = getMouseXYRelative();
     auto mouseWidthRatio = (double) mouseXY.getX() / (double) this->getWidth();
     return mouseWidthRatio;
 }
 
-double WaveformDisplay::getTrackWidthRatio()
-{
-    return (getMouseWidthRatio() * getLocalBounds().getWidth() + (zoomFactor * leftWidthOffset))/
-    ((getLocalBounds().getWidth() + (zoomFactor * rightWidthOffset) + (zoomFactor * leftWidthOffset)));
+double WaveformDisplay::getTrackWidthRatio() {
+    return (getMouseWidthRatio() * getLocalBounds().getWidth() + (zoomFactor * leftWidthOffset)) /
+           ((getLocalBounds().getWidth() + (zoomFactor * rightWidthOffset) + (zoomFactor * leftWidthOffset)));
 }
 
-double WaveformDisplay::map(double x, double lowCurrent, double highCurrent, double lowTarget, double highTarget)
-{
+double WaveformDisplay::map(double x, double lowCurrent, double highCurrent, double lowTarget, double highTarget) {
     auto ratio = (x - lowCurrent) / (highCurrent - lowCurrent);
     return ratio * (highTarget - lowTarget) + lowTarget;
 }
--- WaveformDisplay.h
+++ WaveformDisplay.h
@@ -4,53 +4,131 @@
 #include <JuceHeader.h>
 #include "DJAudioPlayer.h"
 
+/**
+ * A class to display the waveform of the audio track on the DJ deck
+ */
 class WaveformDisplay : public juce::Component,
-                        public juce::ChangeListener
-{
+                        public juce::ChangeListener {
 public:
-    WaveformDisplay(DJAudioPlayer* player,
+
+    /**
+     * WaveformDisplay constructor
+     * @param player handles the audio functionality of the DJ deck
+     * @param formatManagerToUse keeps a list of available audio formats, and decides which one to use to open a given file
+     * @param cacheToUse used to manage multiple AudioThumbnail objects
+     */
+    WaveformDisplay(DJAudioPlayer *player,
                     AudioFormatManager &formatManagerToUse,
                     AudioThumbnailCache &cacheToUse);
+
+    /**
+     * WaveformDisplay destructor
+     */
     ~WaveformDisplay() override;
 
-    void loadURL(const URL& audioURL);
+    /**
+     * Loads the given URL into the audioThumbnail
+     * @param audioURL Represents a URL
+     */
+    void loadURL(const URL &audioURL);
 
     /**
      * set the relative position of the playhead
-     * @param pos
+     * @param pos relative position
      */
     void setPositionRelative(double pos);
 
     /**
      * gets the relative cue position set on the waveform of the track
-     * @return
+     * @return relative position of the cue
      */
     double getCuePositionRelative() const;
 
-    //================ Component functions ================
+    //================ Component override functions ================
+
+    /**
+     * Called when a mouse button is pressed
+     * @param event Contains position and status information about a mouse event
+     */
     void mouseDown(const MouseEvent &event) override;
-    void mouseWheelMove(const MouseEvent& event, const MouseWheelDetails& wheel) override;
+
+    /**
+     * Called when the mouse-wheel is moved
+     * @param event Contains position and status information about a mouse event
+     * @param wheel Contains status information about a mouse wheel event
+     */
+    void mouseWheelMove(const MouseEvent &event, const MouseWheelDetails &wheel) override;
+
     //================ Component pure virtual functions ================
-    void paint(Graphics& g) override;
+
+    /**
+     * Components can override this method to draw their content
+     * @param g the graphics context that must be used to do the drawing operations
+     */
+    void paint(Graphics &g) override;
+
+    /**
+     * Called when this component's size has been changed
+     */
     void resized() override;
 
     //================ ChangeListener pure virtual functions ================
+
+    /**
+     * implement this method to receive the callback
+     * @param source the ChangeBroadcaster that triggered the callback
+     */
     void changeListenerCallback(ChangeBroadcaster *source) override;
 
 private:
 
+    /**
+     * Gets the ratio: current mouse X position / component width
+     * @return mouse width ratio
+     */
     double getMouseWidthRatio();
+
+    /**
+     * Gets the track width ratio
+     * @return track width ratio
+     */
     double getTrackWidthRatio();
+
+    /**
+     * performs a linear mathematical map of a value from one range to another
+     * @param x value to map
+     * @param lowCurrent lower value of current range
+     * @param highCurrent higher value of current range
+     * @param lowTarget lower value of the range intended to map to
+     * @param highTarget higher value of the range intended to map to
+     * @return mapped value
+     */
     static double map(double x, double lowCurrent, double highCurrent, double lowTarget, double highTarget);
 
-    DJAudioPlayer* player;
+    // handles the audio functionality of a DJ deck
+    DJAudioPlayer *player;
+
+    // keeps a list of available audio formats, and decides which one to use to open a given file
     AudioThumbnail audioThumbnail;
+
+    // boolean representing whether the file has loaded
     bool fileLoaded;
+
+    // current track position
     double position{0.0};
+
+    // current cue position
     double cuePosition{0.0};
+
+    // fator to multiply by when zoom is enabled
     double zoomFactor{0.0};
+
+    // the amount to offset the waveform from the left when zooming
     double leftWidthOffset{0.0};
+
+    // the amount to offset the waveform from the right when zooming
     double rightWidthOffset{0.0};
+
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(WaveformDisplay)
 };
 
